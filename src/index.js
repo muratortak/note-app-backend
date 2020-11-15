42c31ccc67790d06fcdcbda76f752ff473ac6e6d
murat ortak <murat.ortak35@hotmail.com>
2020-11-15 04:09:52 -0500

    update routes

--- src/index.js
+++ src/index.js
@@ -29,8 +29,8 @@ dotenv.config();
 app.use(express.json({limit: '10mb'}));
 app.use(cors(corsOptions));
 app.use(cookieParser());
-app.use('/api/user', userRouter);
-app.use('/api/note', noteRouter);
+app.use('/user', userRouter);
+app.use('/note', noteRouter);
 
 try {
   mongoose.connect(url + process.env.DB_NAME, {useNewUrlParser: true});
